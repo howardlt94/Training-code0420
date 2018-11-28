8d4763f1f27fdb204875e450be37d672494415da
Howard Tran <howardlt94@gmail.com>
2018-11-28 17:08:02 -0600

    add sfghkj

--- week1/day3/ANIMALS/Animals.Library/Eagle.cs
+++ week1/day3/ANIMALS/Animals.Library/Eagle.cs
@@ -1,6 +1,6 @@
 namespace Animals.Library
 {
-    public class Eagle : ABird, IAnimal
+    public class Eagle : ABird 
     {
         public override string Name { get;set;}
         public override void MakeSound()
